fca372cae270656b5d4839a9cd0845a45ea95788
Sactio Swastioyono <email.tyo@gmail.com>
2020-08-12 14:43:58 +0900

    init commit

---
+++ src/main/java/com/test/soccerapi/SoccerApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.soccerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerApiApplication.class, args);
+	}
+
+}
